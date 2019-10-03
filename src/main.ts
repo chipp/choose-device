f1fd199c3fc4b809edc644802404f4100dc82bd6
Vladimir Burdukov <vladimir_burdukov@epam.com>
2019-10-03 22:27:09 +0300

    Initial version

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,23 @@
-import * as core from '@actions/core';
-import {wait} from './wait'
+import * as core from "@actions/core";
+import { find_device } from "./find_device";
 
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+    let os = core.getInput("os");
+    let os_type = os.split(" ")[0];
 
-    core.debug((new Date()).toTimeString())
-    await wait(parseInt(ms));
-    core.debug((new Date()).toTimeString())
-
-    core.setOutput('time', new Date().toTimeString());
+    switch (os_type) {
+      case "macOS":
+        core.setOutput("destination", "platform=macOS,arch=x86_64");
+        break;
+      case "iOS":
+      case "tvOS":
+        var destination = await find_device(os);
+        core.setOutput("destination", destination);
+        break;
+      default:
+        core.setFailed("unknown OS type " + os_type);
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
